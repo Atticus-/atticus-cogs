8a9c0f56b0ee898e7aae6fba32f7b1116ad0baab
Jay B <system.err.print@gmail.com>
2017-02-15 23:59:25 -0800

    Working .togglerole command

--- rolemanagement/rolemanagement.py
+++ rolemanagement/rolemanagement.py
@@ -7,14 +7,20 @@ class RoleManagement:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(pass_context=True)
     async def togglerole(self, ctx, role : discord.Role):
-        """Allows users to toggle a role on themselves."""
+        """Allows users to toggle a role on themselves.  You don't have to @mention the role, just type the name. Example: .togglerole lfg"""
 
         #Your code will go here
-        already_has_role = role in ctx.message.author.roles
-        await self.bot.say("Already in role? "+already_has_role)
+        try:
+            if role in ctx.message.author.roles:
+                await self.bot.remove_roles(ctx.message.author, role)
+                await self.bot.say("Removed role "+str(role)+" from "+ctx.message.author.mention)
+            else:
+                await self.bot.add_roles(ctx.message.author, role)
+                await self.bot.say("Added role "+str(role)+" to "+ctx.message.author.mention)
+        except discord.Forbidden:
+            await self.bot.say("I don't have permissions to manage that role.")
 
 def setup(bot):
     bot.add_cog(RoleManagement(bot))
-
